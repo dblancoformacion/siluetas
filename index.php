0bddbea978239444c16f1093e278f7f9fa7262aa
dblancoformacion <dblancoformacion@gmail.com>
2019-06-14 21:21:38 +0200

    Dos chicas, evolución simultánea, persistencia en la URL, POO preliminar

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <?php
-// controlador
-if(isset($_GET['estado']))
-	$_GET['estado']++;
-else $_GET['estado']=1;
-
-if($_GET['estado']>7)
-	$_GET['estado']=1;
-
-echo mostrar();
-echo mostrar();
-
-// vista
-function mostrar(){
-	return '
-		<div>
-			<a href="?estado='.$_GET['estado'].'">
-				<img src="figs/f'.$_GET['estado'].'.jpg" style="height:300px">
-			</a>
-		</div>		
-	';
+Class Siluetas{
+	// modelo
+	public $estado=1;
+	function __construct(){
+		if(isset($_GET['estado']))
+			$this->estado=$_GET['estado'];
+	}
+	// vista
+	function mostrar(){
+		return '
+			<div>
+				<a href="?atras=1&estado='.$this->estado.'" style="font-size:4em"> < </a>
+				<a href="?estado='.$this->estado.'" style="font-size:4em">
+					<img src="figs/f'.$this->estado.'.jpg" style="height:300px;"> >
+				</a>
+			</div>		
+		';
+	}	
 }
+
+// controlador
+$s=new Siluetas();
+if(isset($_GET['estado']) && !isset($_GET['atras']))
+	$s->estado++;
+if($s->estado>7)
+	$s->estado=1;
+if(isset($_GET['atras']))
+	$s->estado--;
+if($s->estado<1)
+	$s->estado=7;
+echo $s->mostrar();
+echo $s->mostrar();
+
 ?>
