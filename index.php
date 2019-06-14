3e6a79c98d0a86b311c6e617b2fe94036d138f3b
dblancoformacion <dblancoformacion@gmail.com>
2019-06-14 20:46:21 +0200

    Una chica, persistencia en la URL

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-Siluetas
-
 <?php
 
+if(isset($_GET['silueta']))
+	$_GET['silueta']++;
+else $_GET['silueta']=1;
 
+if($_GET['silueta']>7)
+	$_GET['silueta']=1;
 
+echo '
+	<a href="?silueta='.$_GET['silueta'].'">
+		<img src="figs/f'.$_GET['silueta'].'.jpg">
+	</a>
+';
 
 ?>
